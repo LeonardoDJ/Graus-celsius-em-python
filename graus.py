361bc26438d591eb4618b6805b7ac8cbd8be6cb6
Leonardo_DJ <serlepe147leo@gmail.com>
2024-09-20 08:30:48 -0300

    Add files via upload
    Umas das primeiras atividades em python  que eu fiz

---
+++ graus.py
@@ -0,0 +1,5 @@
+celsius = int(input("Digite uma temperatura em graus C°: "))
+def fahrenheint(celsius):
+    return(celsius * 1.8) + 32
+print("\n")
+print("A temperatura de", celsius, "C° é equivalente à", fahrenheint(celsius), "F°")
